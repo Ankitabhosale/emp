7ebea16fe4089478f2bb928a4afac4231b28b8b5
dac <dac@dypm.org>
2019-06-26 08:04:06 +0530

    first commit

--- emp.js
+++ emp.js
@@ -4,24 +4,24 @@ var app=express();
 
 var DetailsController=function(req,res){
 
-console.log("details are following");
+console.log("Contoso");
 var details=
 [
-{OfficeId:1,Location:"Bangalore", Name: "Ankita", product:"software", Account:"Saving"},
-{OfficeId:2,Location:"Pune", Name: "Sayali", product:"technical support",Account:"Current"},
-{OfficeId:3,Location:"Mumbai",  Name: "Megha", product:"hardware", Account:"Saving"},
-{OfficeId:4,Location:"Chennai", Name: "Harshada", product:"BPO", Account:"Current"},
-{OfficeId:5,Location:"TN", Name: "Shweta", product:"Testing", Account:"Current"},
+{OfficeId:1,Location:"Bangalore", Name: "Ankita", description:"software", Account:"Saving"},
+{OfficeId:2,Location:"Pune", Name: "Sayali", description:"technical support",Account:"Current"},
+{OfficeId:3,Location:"Mumbai",  Name: "Megha", description:"hardware", Account:"Saving"},
+{OfficeId:4,Location:"Chennai", Name: "Harshada", description:"BPO", Account:"Current"},
+{OfficeId:5,Location:"TN", Name: "Shweta", description:"Testing", Account:"Current"},
 ];
 res.send(details);
 };
 
 app.get('/details',DetailsController);
 
-var server=app.listen(8088,function(req,res)
+var server=app.listen(3000,function(req,res)
 {
 	
-console.log("details are provided by http://localhost:8088");	
+console.log("details are provided by http://localhost:3000");	
 }
 )
 
